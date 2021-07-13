08a6840d7c3ee4da57e0e7e2ab4a2bc537792334
skep <skep@freq>
2021-07-13 14:49:10 +0200

    add intl/*.pw to brain & `brain ls <region>`

--- brain.sh
+++ brain.sh
@@ -8,10 +8,11 @@ source "$cwd/grep.sh"
 __brain_find_file_in_path () {
   #echo "[d] __brain_find_file_in_path $@" >&2
   local root="$1" q="$2"
-  local f=($(find "$root" -name "lang.$q" -or -name "$q.lang"))
+  local f=($(find "$root" -name "$__brain_suffix.$q" -or -name "$q.$__brain_suffix")) # try exact matches first
   if [[ $#f -eq 0 ]]; then
-    f=($(find "$root" -name "*$q*.$__brain_suffix" -or -name "$__brain_suffix.*$q*"))
+    f=($(find "$root" -name "*$q*.$__brain_suffix" -or -name "$__brain_suffix.*$q*")) # try non-complete matches
   fi
+  # TODO: fuzzy machtes?
   echo "${(j/:/)f}"
   [[ $#f -eq 1 ]] || return 1
   return 0
@@ -35,7 +36,7 @@ __brain_find_file () {
   fi
   return 0
 }
-__brain_edit () {
+__brain_root_edit () {
   if [[ $# -eq 0 ]]; then
     echo "brain: no search term provided"
     return 1
@@ -55,20 +56,43 @@ __brain_edit () {
   fi
   "$EDITOR" "$f"
 }
+__brain_pw_roots=($HOME/z/priv/misc/intl)
+__brain_pw_suffix=pw
+__brain_pw_edit () {
+  local __brain_roots=$__brain_pw_roots __brain_suffix=$__brain_pw_suffix
+  __brain_root_edit "$@"
+}
+__brain_pw_ls () {
+  ls --color=auto -lhB $__brain_pw_roots
+}
+__brain_ls () {
+  local f=__brain_"$1"_ls
+  if functions|grep -q $f; then
+    $f
+  else
+    echo "brain: not found"
+  fi
+}
 brain () {
   if [[ $# -eq 0 ]]; then
     echo "brain"
-    echo "  e|edit <file>     search for <file> an open it"
+    echo "  e|edit <file>     search brain for lan.<file>/<file.lang an open it"
     echo "  n|new <file>      ???"
+    echo "  pw|intl <file>    search brain for <file>.pw"
+    echo "  l|ls <region>       list brain <region> content"
     return 0
   fi
   local arg1="$1"
   shift
   if [[ "$arg1" =~ '^(e|edit)$' ]]; then
-    __brain_edit "$@"
+    __brain_root_edit "$@"
   elif [[ "$arg1" =~ '^(g|grep)$' ]]; then
     greplang "$@"
+  elif [[ "$arg1" =~ '^(pw|intl)$' ]]; then
+    __brain_pw_edit "$@"
+  elif [[ "$arg1" =~ '^(l|ls)$' ]]; then
+    __brain_ls "$@"
   elif [[ "$arg1" =~ '^(n|new)$' ]]; then
-    echo "hippocamus: long term memory failure"
+    echo "hippocampus: long term memory failure"
   fi
 }
