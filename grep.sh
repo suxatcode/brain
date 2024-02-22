65c162069ea0f302a53415e76a18f245e696e3f0
suxatcode <nnamegah21@gmx.de>
2024-02-22 11:48:22 +0100

    chore: `brain grep` prefers fd over find

--- grep.sh
+++ grep.sh
@@ -10,7 +10,9 @@ _grepLangGrep () {
 _grepLangInit () {
   _grepLangSearchPATH=( "$@" )
 }
-_grepFindFilesToGrep () {
+
+# TODO: move to find.sh
+_grepFindFilesToGrep_find () {
   local IFS=$'\n'
   local findargs=(
     -type f
@@ -18,13 +20,27 @@ _grepFindFilesToGrep () {
     -not -wholename '*/docker-data/*'
     -not -wholename '*/node_modules/*'
   )
-  # TODO: reuse find.sh functions
   local langfiles=($(find $_grepLangSearchPATH -regex ".*/$__brain_suffix"'[^/]*[^~]$\|.*'"$__brain_suffix$" $findargs))
   local todos=($(find $_grepLangSearchPATH -name "todo" -type f))
   for t in $todos; do langfiles+=$t; done
-  #echo "XXX $langfiles" >&2
   echo "${langfiles[@]}"
 }
+_grepFindFilesToGrep_fd () {
+  local opts=($(printf ' -E "%s" ' "${__brain_directory_ignores[@]}"))
+  local f=($(fd $opts "(^$__brain_suffix\.|\.$__brain_suffix$)" $_grepLangSearchPATH))
+  if [[ $#f -eq 0 ]]; then
+    f=($(fd $opts "(^${__brain_suffix}\.[^/]*|[^/]*\.${__brain_suffix})" $_grepLangSearchPATH))
+  fi
+  echo "${f}"
+}
+_grepFindFilesToGrep () {
+  if which fd >/dev/null; then
+    _grepFindFilesToGrep_fd
+  else
+    _grepFindFilesToGrep_find
+  fi
+}
+
 _grepLangAndTodoFiles () {
   _grepLangGrep "$@" $(_grepFindFilesToGrep)
 }
